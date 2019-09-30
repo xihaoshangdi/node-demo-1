91538206bbb4dc0c99611ffeefe89c3773b8489d
xihaoshangdi <xihaoshangdi@163.com>
2019-09-30 11:03:32 +0800

    更新了错误的路由

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ var server = http.createServer(function(request, response) {
       <h1>你好</h1>
     `);
     response.end();
-  } else if (path === './style.css') {
+  } else if (path === '/style.css') {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/css;charset=utf-8');
     response.write(`h1{color: red;}`);
