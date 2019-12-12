4a7e912a63308ad1e6d0502a1ec2998dd22c45de
xihaoshangdi <xihaoshangdi@163.com>
2019-12-12 09:50:29 +0800

    重写了静态服务器，支持了多种格式和默认页面。

--- server.js
+++ server.js
@@ -23,23 +23,30 @@ var server = http.createServer(function(request, response) {
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery);
 
-  if (path === '/') {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/html;charset=utf-8');
-    response.write(`
-      <link rel="stylesheet" href="./style.css">
-      <h1>你好</h1>
-    `);
-    response.end();
-  } else if (path === '/style.css') {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/css;charset=utf-8');
-    response.write(`h1{color: red;}`);
-    response.end();
-  } else {
+  response.statusCode = 200;
+
+  const defaultPath = path === '/' ? '/index.html' : path;
+  const index = defaultPath.indexOf('.');
+  const suffix = defaultPath.substr(index);
+  const fileTypes = {
+    '.html': 'text/html;charset=utf-8',
+    '.css': 'text/css',
+    '.js': 'text/javascript;charset=utf-8',
+    '.png': 'image/png;charset=utf-8',
+    '.jpg': 'image/jpeg;charset=utf-8',
+    '.mp3': 'audio/mpeg'
+  };
+  response.setHeader('Content-Type', `${fileTypes[suffix]}`);
+  let content;
+  try {
+    content = fs.readFileSync(`./public${defaultPath}`);
+  } catch (error) {
+    content = 'error';
     response.statusCode = 404;
-    response.end();
   }
+
+  response.write(content);
+  response.end();
 
   /******** 代码结束，下面不要看 ************/
 });
